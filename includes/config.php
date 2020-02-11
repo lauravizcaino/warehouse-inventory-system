b60262c7f56b37ec5810e012f63e836391c80356
Laura <lauraviz_00@hotmail.com>
2020-02-11 15:32:09 -0500

    database settings

--- includes/config.php
+++ includes/config.php
@@ -13,8 +13,8 @@
 |
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'admin' );             // Set database user
-  define( 'DB_PASS', 'root' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  define( 'DB_USER', 'laura' );             // Set database user
+  define( 'DB_PASS', 'flor.123' );             // Set database password
+  define( 'DB_NAME', 'inventario' );        // Set database name
 //kjfjvdgv
 ?>
