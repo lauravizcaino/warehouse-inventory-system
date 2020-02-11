2fb468e73c3a545b0a5d57a0f2cd912cd98664a3
Laura <lauraviz_00@hotmail.com>
2020-02-11 15:24:18 -0500

    my first commit

--- includes/config.php
+++ includes/config.php
@@ -16,5 +16,5 @@
   define( 'DB_USER', 'admin' );             // Set database user
   define( 'DB_PASS', 'root' );             // Set database password
   define( 'DB_NAME', 'oswa_inv' );        // Set database name
-
+//kjfjvdgv
 ?>
