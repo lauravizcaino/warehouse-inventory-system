d55e679559297da0c1b3c4e70dc268e490ecb238
Laura <lauraviz_00@hotmail.com>
2020-02-13 10:45:49 -0500

    modificar tabla de productos

--- product.php
+++ product.php
@@ -57,9 +57,9 @@
                 <?php endif; ?>
                 </td>
                 <td> <?php echo remove_junk($product['name']); ?></td>
-                <!--
-                <td class="text-center"> <?php /*echo remove_junk($product['categorie']);*/ ?></td>
-                <td class="text-center"> <?php /*echo remove_junk($product['quantity']);*/ ?></td>
+                
+                <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
+                <!--<td class="text-center"> <?php /*echo remove_junk($product['quantity']);*/ ?></td>
                 <td class="text-center"> <?php /*echo remove_junk($product['buy_price']); */?></td>
                 <td class="text-center"> <?php /*echo remove_junk($product['sale_price']);*/ ?></td>
                 <td class="text-center"> <?php /*echo read_date($product['date']); */?></td>
