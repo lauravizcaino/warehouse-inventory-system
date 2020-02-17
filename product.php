cc7627f28b7182f9298000f95a400ea0a7107036
Laura <lauraviz_00@hotmail.com>
2020-02-17 15:47:21 -0500

    modificar add_product añadir campos

--- product.php
+++ product.php
@@ -49,13 +49,13 @@
               <?php foreach ($products as $product):?>
               <tr>
                 <td class="text-center"><?php echo count_id();?></td>
-                <td>
-                  <?php if($product['media_id'] === '0'): ?>
+                <!--<td>
+                  <?php /*if($product['media_id'] === '0'): */?>
                     <img class="img-avatar img-circle" src="uploads/products/no_image.jpg" alt="">
-                  <?php else: ?>
-                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $product['image']; ?>" alt="">
-                <?php endif; ?>
-                </td>
+                  <?php /*else:*/ ?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php /*echo $product['image']; */?>" alt="">
+                <?php /*endif; */?>
+                </td>-->
                 <td> <?php echo remove_junk($product['name']); ?></td>
                 
                 <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
