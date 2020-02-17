85987497ab011b61d239ab876e77b0d234f7d187
Laura <lauraviz_00@hotmail.com>
2020-02-17 16:06:07 -0500

    modificar add_product añadir campos

--- add_product.php
+++ add_product.php
@@ -92,7 +92,7 @@
                       <option value="">Seleccione el tipo de bien</option>
                     <?php  foreach ($all_categories as $cat): ?>
                       <option value="<?php echo (int)$cat['id'] ?>">
-                        <?php echo $cat['name'] ?></option>
+                        <?php echo $cat['categorie'] ?></option>
                     <?php endforeach; ?>
                     </select>
                   </div>
