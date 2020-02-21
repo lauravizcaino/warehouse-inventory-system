bceed638d4ac365be0a4160f086ff3869dc6012a
Laura <lauraviz_00@hotmail.com>
2020-02-21 15:11:59 -0500

    modificar index

--- index.php
+++ index.php
@@ -6,21 +6,21 @@
 <?php include_once('layouts/header.php'); ?>
 <div class="login-page">
     <div class="text-center">
-       <h1>Welcome</h1>
-       <p>Sign in to start your session</p>
+       <h1>Bienvenido</h1>
+       <p>Inicia sesión para comenzar</p>
      </div>
      <?php echo display_msg($msg); ?>
       <form method="post" action="auth.php" class="clearfix">
         <div class="form-group">
-              <label for="username" class="control-label">Username</label>
-              <input type="name" class="form-control" name="username" placeholder="Username">
+              <label for="username" class="control-label">Nombre de Usuario</label>
+              <input type="name" class="form-control" name="username" placeholder="Usuario">
         </div>
         <div class="form-group">
-            <label for="Password" class="control-label">Password</label>
-            <input type="password" name= "password" class="form-control" placeholder="password">
+            <label for="Password" class="control-label">Contraseña</label>
+            <input type="password" name= "password" class="form-control" placeholder="Contraseña">
         </div>
         <div class="form-group">
-                <button type="submit" class="btn btn-info  pull-right">Login</button>
+                <button type="submit" class="btn btn-info  pull-right">Iniciar sesión</button>
         </div>
     </form>
 </div>
