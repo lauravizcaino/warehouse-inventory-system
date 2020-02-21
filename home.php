793e00296b037972faad31d0cfa7c254b57145b5
Laura <lauraviz_00@hotmail.com>
2020-02-21 15:23:34 -0500

    modificar home

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-  $page_title = 'Home Page';
+  $page_title = 'Home';
   require_once('includes/load.php');
   if (!$session->isUserLoggedIn(true)) { redirect('index.php', false);}
 ?>
@@ -11,8 +11,11 @@
  <div class="col-md-12">
     <div class="panel">
       <div class="jumbotron text-center">
-         <h1>This is your new home page!</h1>
-         <p>Just browes around and find out what page you can access.</p>
+         <h1>Sistema de Inventario</h1>
+         <h2>Facultad de Ingeniería Eléctrica y Electrónica</h2>
+         <h3>Laboratorio de Informática</h3>
+         <img src="https://www.epn.edu.ec/wp-content/uploads/2014/05/EPN_logo-250x152.jpg" alt="Escudo EPN">
+         <!--<p>Just browes around and find out what page you can access.</p>-->
       </div>
     </div>
  </div>
