5cf39c765976b2e5e3866ca7ae7c521f206f8835
Laura <lauraviz_00@hotmail.com>
2020-02-22 15:44:23 -0500

    modificar categorie

--- product.php
+++ product.php
@@ -55,7 +55,7 @@
                   <img class="img-avatar img-circle" src="uploads/products/<?php /*echo $product['image']; */?>" alt="">
                 <?php /*endif; */?>
                 </td>-->
-                <td> <?php echo remove_junk($product['codigo_nfc']); ?></td>
+                <td> <?php echo remove_junk($product['name']); ?></td>
                 
                 <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
                 <!--<td class="text-center"> <?php /*echo remove_junk($product['quantity']);*/ ?></td>
