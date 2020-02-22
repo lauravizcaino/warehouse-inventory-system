b32923cb23dca0211ab56dc094d0f6be990eb6f1
Laura <lauraviz_00@hotmail.com>
2020-02-22 14:56:47 -0500

    modificar categorie

--- edit_categorie.php
+++ edit_categorie.php
@@ -1,5 +1,5 @@
 <?php
-  $page_title = 'Edit categorie';
+  $page_title = 'Editar tipo';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
   page_require_level(1);
