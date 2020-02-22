77f7d3b2ea2b8470c3874447414ebf65f5558144
Laura <lauraviz_00@hotmail.com>
2020-02-22 16:32:47 -0500

    modificar categorie

--- product.php
+++ product.php
@@ -62,7 +62,7 @@
                 <td class="text-center"> <?php /*echo remove_junk($product['sale_price']);*/ ?></td>
                 <td class="text-center"> <?php /*echo read_date($product['date']); */?></td>
                 -->
-
+                <td class="text-center"> <?php echo remove_junk($product['name']); ?></td>
                 <td class="text-center"> <?php echo remove_junk($product['codigo_nfc']); ?></td>
                 <td class="text-center"> <?php echo remove_junk($product['serial']); ?></td>
                 <td class="text-center"> <?php echo remove_junk($product['codigo_inventario']); ?></td>
