f9514361b72795fb6908c98fab3edaa1a7dae302
Laura <lauraviz_00@hotmail.com>
2020-02-22 15:54:19 -0500

    modificar categorie

--- product.php
+++ product.php
@@ -23,7 +23,7 @@
               <tr>
                 <th class="text-center" style="width: 50px;">#</th>
                 <!--<th> Photo</th>-->
-                <th> Nombre</th>
+                <th class="text-center" style="width: 10%;"> Nombre</th>
                 <th class="text-center" style="width: 10%;"> Tipo de bien </th>
                 <!--<th class="text-center" style="width: 10%;"> Instock </th>
                 <th class="text-center" style="width: 10%;"> Buying Price </th>
@@ -55,9 +55,8 @@
                   <img class="img-avatar img-circle" src="uploads/products/<?php /*echo $product['image']; */?>" alt="">
                 <?php /*endif; */?>
                 </td>-->
-                <td> <?php echo remove_junk($product['name']); ?></td>
-                
                 <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['name']); ?></td>
                 <!--<td class="text-center"> <?php /*echo remove_junk($product['quantity']);*/ ?></td>
                 <td class="text-center"> <?php /*echo remove_junk($product['buy_price']); */?></td>
                 <td class="text-center"> <?php /*echo remove_junk($product['sale_price']);*/ ?></td>
