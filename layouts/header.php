a0e721e4a3029cf8b6642a1bdeb94955e913262f
Laura <lauraviz_00@hotmail.com>
2020-02-22 14:36:26 -0500

    modificar header

--- layouts/header.php
+++ layouts/header.php
@@ -16,7 +16,7 @@
   <body>
   <?php  if ($session->isUserLoggedIn(true)): ?>
     <header id="header">
-      <div class="logo pull-left"> Laura </div>
+      <div class="logo pull-left"> Opciones </div>
       <div class="header-content">
       <div class="header-date pull-left">
         <strong><?php echo date("F j, Y, g:i a");?></strong>
@@ -32,19 +32,19 @@
               <li>
                   <a href="profile.php?id=<?php echo (int)$user['id'];?>">
                       <i class="glyphicon glyphicon-user"></i>
-                      Profile
+                      Perfil
                   </a>
               </li>
              <li>
                  <a href="edit_account.php" title="edit account">
                      <i class="glyphicon glyphicon-cog"></i>
-                     Settings
+                     Ajustes
                  </a>
              </li>
              <li class="last">
                  <a href="logout.php">
                      <i class="glyphicon glyphicon-off"></i>
-                     Logout
+                     Salir
                  </a>
              </li>
            </ul>
