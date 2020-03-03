da644242e941253ccd5d9ec970ae350903b9188c
Laura <lauraviz_00@hotmail.com>
2020-03-03 10:51:54 -0500

    fecha

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,6 @@
 <?php
  	
- date_default_timezone_set("America/Lima");
+ //date_default_timezone_set("America/Lima");
  $errors = array();
 
  /*--------------------------------------------------------------*/
