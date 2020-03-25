9905f1720c76db1fcc288eeec7c8ec3b3380babb
Laura Vizcaíno <lauraviz_00@hotmail.com>
2020-03-25 14:43:45 -0500

    DB

--- includes/config.php
+++ includes/config.php
@@ -15,6 +15,6 @@
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'laura' );             // Set database user
   define( 'DB_PASS', 'flor.123' );             // Set database password
-  define( 'DB_NAME', 'inventario' );        // Set database name
+  define( 'DB_NAME', 'Inventario' );        // Set database name
 //kjfjvdgv
 ?>
