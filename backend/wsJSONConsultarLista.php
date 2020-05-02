328298d70b1ceddaec5fb90ed7527b12c98ee096
Laura Vizcaíno <lauraviz_00@hotmail.com>
2020-05-02 17:40:49 -0500

    DB

--- backend/wsJSONConsultarLista.php
+++ backend/wsJSONConsultarLista.php
@@ -9,14 +9,14 @@ $json=array();
 				
 		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-		$consulta="select name, codigo_nfc, serial, fecha_garantia, codigo_inventario, custodio, fecha_ingreso, ubicacion,fecha_ultimo_mantenimiento,fecha_compra, marca , procesador, estado, caracteristicas,tipo FROM products";
+		$consulta="select name, codigo_nfc, serial, fecha_garantia, codigo_inventario, custodio, fecha_ingreso, ubicacion,fecha_ultimo_mantenimiento,fecha_compra, marca , procesador, estado, caracteristica,tipo FROM products";
 		$resultado=mysqli_query($conexion,$consulta);
 		
 		while($registro=mysqli_fetch_array($resultado)){
 			$json['products'][]=$registro;
 		}
 		mysqli_close($conexion);
-		//echo json_encode($json,JSON_UNESCAPED_UNICODE);
-		echo ($json);
+		echo json_encode($json,JSON_UNESCAPED_UNICODE);
+		//echo ($json);
 ?>
 
