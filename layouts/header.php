b14732c9aa7467463edf8e8392458500b0286365
Laura Vizcaíno <lauraviz_00@hotmail.com>
2020-06-27 16:19:39 -0500

    export

--- layouts/header.php
+++ layouts/header.php
@@ -11,6 +11,7 @@
            echo ucfirst($user['name']);
             else echo "Sistema de Inventario";?>
     </title>
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" />
