89bc17c3d29de87475c5a1733115a8817aac9e6b
Laura Vizcaíno <lauraviz_00@hotmail.com>
2020-06-27 16:35:38 -0500

    export

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,5 +1,6 @@
      </div>
     </div>
+    <script src=http://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script src="libs/js/tableToExcel.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
